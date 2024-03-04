081665b76fd29dff429970a281d45090a1990a33
Patrick Jade Pelicano <61832441+thejadenode@users.noreply.github.com>
2024-03-04 18:40:06 +0800

    moved reusable components import to main js
    this removes manual components import on every page

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,7 @@ import '../scss/styles.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 import "bootstrap-icons/font/bootstrap-icons.css"
+
+import '/js/nav.js'
+import '/js/footer.js'
+import '/js/placeholder.js'
