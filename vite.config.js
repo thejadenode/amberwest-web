cd4b16f4f4fb183d24af58b4bf5f5f775c6892bd
Patrick Jade Pelicano <61832441+thejadenode@users.noreply.github.com>
2024-03-04 18:57:48 +0800

    installed and configured handlebars plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { resolve } from 'path'
+import handlebars from 'vite-plugin-handlebars'
 
 export default {
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, 'src/partials'),
+    }),
+  ],
   root: resolve(__dirname, 'src'),
   build: {
     outDir: '../dist'
